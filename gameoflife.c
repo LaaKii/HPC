920193dc4cce84c14931951923a44ce239a29ee1
LaaKii <kai-achertal@gmx.de>
2021-03-02 23:48:31 -0800

    initial commit

--- gameoflife.c
+++ gameoflife.c
@@ -14,35 +14,63 @@
 
 //long TimeSteps = 100;
 
-void writeData(const unsigned int *data, int w, int h, FILE *fp);
-
-void writeFooter(FILE *fp);
-
-FILE *writeHeader(long timestep, const char *prefix, int w, int h);
-
-//void writeVTK2(long timestep, unsigned *data, char prefix[1024], int w, int h) {
-//    FILE *fp = writeHeader(timestep, prefix, w, h);
-//
-//    writeData(data, w, h, fp);
-//
-//    writeFooter(fp);
-//}
-
-FILE *writeHeader(long timestep, const char *prefix, int w, int h) {
+void writeMainVTK(int nx, int ny, int Px, int Py, long timestep) {
     char filename[2048];
 
-    int offsetX = 0;
-    int offsetY = 0;
     float deltax = 1.0;
-    long nxy = w * h * sizeof(float);
 
-    snprintf(filename, sizeof(filename), "%s-%05ld%s", prefix, timestep, ".vti");
+    int width = nx * Px;
+    int height = ny * Py;
+
+    snprintf(filename, sizeof(filename), "%s-%05ld%s", "header", timestep, ".pvti");
+    FILE *fp = fopen(filename, "w");
+
+    fprintf(fp, "<?xml version=\"1.0\"?>\n");
+    fprintf(fp, "<VTKFile type=\"PImageData\" version=\"0.1\" byte_order=\"LittleEndian\" header_type=\"UInt64\">\n");
+    fprintf(fp, "<PImageData WholeExtent=\"%d %d %d %d %d %d\" Origin=\"0 0 0\" Spacing=\"%le %le %le\">\n", 0, width-1, 0, height-1,
+            0, 0, deltax, deltax, 0.0);
+    fprintf(fp, "<PCellData Scalars=\"%s\">\n", "gol");
+    fprintf(fp, "<PDataArray type=\"Float32\" Name=\"%s\" format=\"appended\" offset=\"0\"/>\n", "gol");
+    fprintf(fp, "</PCellData>\n");
+
+    int num_threads = nx * ny;
+    for (int i = 0; i < num_threads; i++) {
+
+        int x_start = (i % nx) * Px;
+        int x_end = x_start + Px;
+        int y_start = (i / nx) * Py;
+        int y_end = y_start + Py;
+
+
+        fprintf(fp, "<Piece Extent=\"%d %d %d %d %d %d\" Source=\"%s-%d-%05ld%s\"/>", x_start, x_end, y_start,
+                y_end, 0, 0, "gol", i, timestep, ".vti");
+    }
+
+    fprintf(fp, "</PImageData>\n");
+    fprintf(fp, "</VTKFile>\n");
+    fclose(fp);
+}
+
+void writeVTK(long timestep, unsigned *data, char prefix[1024], int threadNum, int nx, int ny, int Px, int Py) {
+    char filename[2048];
+
+    int width = nx * Px;
+    int height = ny * Py;
+    float deltax = 1.0;
+    long nxy = width * height * sizeof(float);
+
+    int x_start = (threadNum % nx) * Px;
+    int x_end = x_start + Px;
+    int y_start = (threadNum / nx) * Py;
+    int y_end = y_start + Py;
+
+    snprintf(filename, sizeof(filename), "%s-%d-%05ld%s", prefix, threadNum, timestep, ".vti");
     FILE *fp = fopen(filename, "w");
 
     fprintf(fp, "<?xml version=\"1.0\"?>\n");
     fprintf(fp, "<VTKFile type=\"ImageData\" version=\"0.1\" byte_order=\"LittleEndian\" header_type=\"UInt64\">\n");
-    fprintf(fp, "<ImageData WholeExtent=\"%d %d %d %d %d %d\" Origin=\"0 0 0\" Spacing=\"%le %le %le\">\n", offsetX,
-            offsetX + w, offsetY, offsetY + h, 0, 0, deltax, deltax, 0.0);
+    fprintf(fp, "<ImageData WholeExtent=\"%d %d %d %d %d %d\" Origin=\"0 0 0\" Spacing=\"%le %le %le\">\n", x_start,
+            x_end, y_start, y_end, 0, 0, deltax, deltax, 0.0);
     fprintf(fp, "<CellData Scalars=\"%s\">\n", prefix);
     fprintf(fp, "<DataArray type=\"Float32\" Name=\"%s\" format=\"appended\" offset=\"0\"/>\n", prefix);
     fprintf(fp, "</CellData>\n");
@@ -50,24 +78,19 @@ FILE *writeHeader(long timestep, const char *prefix, int w, int h) {
     fprintf(fp, "<AppendedData encoding=\"raw\">\n");
     fprintf(fp, "_");
     fwrite((unsigned char *) &nxy, sizeof(long), 1, fp);
-    return fp;
-}
 
-void writeFooter(FILE *fp) {
+    for (int y = y_start; y < y_end; y++) {
+        for (int x = x_start; x < x_end; x++) {
+            float value = data[calcIndex(width, x, y)];
+
+            fwrite((unsigned char *) &value, sizeof(float), 1, fp);
+        }
+    }
+
     fprintf(fp, "\n</AppendedData>\n");
     fprintf(fp, "</VTKFile>\n");
     fclose(fp);
 }
-
-//void writeData(const unsigned int *data, int w, int h, FILE *fp) {
-//    int x, y;
-//    for (y = 0; y < h; y++) {
-//        for (x = 0; x < w; x++) {
-//            float value = data[calcIndex(h, x, y)];
-//            fwrite((unsigned char *) &value, sizeof(float), 1, fp);
-//        }
-//    }
-//}
 
 
 void show(unsigned *currentfield, int w, int h) {
@@ -83,7 +106,7 @@ void show(unsigned *currentfield, int w, int h) {
 int countLifingsPeriodic(unsigned *currentfield, int x, int y, int w, int h) {
     int n = 0;
     for (int y1 = y - 1; y1 <= y + 1; y1++) {
-        for (int x1 = x - 1; x1 <= x + 1; x1++) {
+        for (int x1 = x; x1 <= x + 1; x1++) {
             if (currentfield[calcIndex(w, (x1 + w) % w, (y1 + h) % h)]) {
                 n++;
             }
@@ -101,19 +124,23 @@ void evolve(unsigned *currentfield, unsigned *newfield, int nx, int ny, int Px, 
 //    FILE *fp = writeHeader(timestep, "gol", width, height);
     int num_threads = nx * ny;
 
-//    for (int i=0; i<num_threads; i++)
-    #pragma omp parallel num_threads(num_threads)
+    for (int i=0; i<num_threads; i++)
+//    #pragma omp parallel num_threads(num_threads)
     {
-        int i = omp_get_thread_num();
+//        int i = omp_get_thread_num();
 
         int x_start = (i % nx) * Px;
         int x_end = x_start + Px;
         int y_start = (i / nx) * Py;
         int y_end = y_start + Py;
 
-//        printf("Thread %d: x(%%d)\n", i, x_start, y_start);
-for (int x = x_start; x < x_end; ++x) {
-            for (int y = y_start; y < y_end; ++y) {
+        if (i == 0) {
+            writeMainVTK(nx, ny, Px, Py, timestep);
+        }
+
+        printf("Thread %d: x(%d-%d) y(%d-%d)\n", i, x_start, x_end, y_start, y_end);
+        for (int y = y_start; y < y_end; ++y) {
+            for (int x = x_start; x < x_end; ++x) {
                 int n = countLifingsPeriodic(currentfield, x, y, width, height);
                 if (currentfield[calcIndex(width, x, y)]) n--;
                 newfield[calcIndex(width, x, y)] = (n == 3 || (n == 2 && currentfield[calcIndex(width, x, y)]));
@@ -122,6 +149,7 @@ for (int x = x_start; x < x_end; ++x) {
 
             }
         }
+        writeVTK(timestep, currentfield, "gol", i, nx, ny, Px, Py);
     }
 
 //    writeFooter(fp);
